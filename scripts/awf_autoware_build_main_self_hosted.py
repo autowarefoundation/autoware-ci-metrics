c313fc0468739ebfd0ddfd2713a01b0e3163fa32
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-02-27 14:37:45 +0900

    Delete debug message

--- scripts/awf_autoware_build_main_self_hosted.py
+++ scripts/awf_autoware_build_main_self_hosted.py
@@ -112,7 +112,6 @@ for run in workflow_runs:
     if run["id"] != 8004157978:
         continue
 
-    print(logs.keys())
     if BUILD_LOG_ID in logs.keys():
         build_log_text = logs[BUILD_LOG_ID]
     elif BUILD_LOG_ID_2 in logs.keys():
