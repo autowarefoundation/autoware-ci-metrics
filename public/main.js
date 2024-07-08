34c5672735916215c1009b75323ca5c3f4585319
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-07-08 17:20:24 +0900

    feat: disable package logging (#18)
    * remove package logs
    
    * change height
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    ---------
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- public/main.js
+++ public/main.js
@@ -11,154 +11,6 @@ fetch('github_action_data.json')
     const mmss = (seconds) =>
       `${Math.ceil(seconds / 60)}m${(seconds % 60).toFixed(0)}s`;
 
-    // Package duration chart
-    const allPackageDurationOptions = {
-      series: [],
-      chart: {
-        height: 500,
-        type: 'donut',
-        zoom: {
-          enabled: true,
-        },
-        selection: {
-          enabled: true,
-        },
-        events: {
-          click: (_event, _chartContext, config) => {
-            const dataPoint = config.dataPointIndex;
-            if (dataPoint === undefined) {
-              return;
-            }
-          },
-        },
-        animations: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      title: {
-        text: 'All package build duration',
-        align: 'left',
-      },
-      tooltip: {
-        y: {
-          formatter: mmss,
-        },
-      },
-    };
-
-    const allPackageDurationChart = new ApexCharts(
-      document.querySelector('#all-package-time-chart'),
-      allPackageDurationOptions,
-    );
-    allPackageDurationChart.render();
-
-    // Handler
-    const showAllPackageDuration = (buildIndex) => {
-      const packageDetails = validatedWorkflowTime[buildIndex].details ?? {};
-      const packageLabels = Object.keys(packageDetails).sort(
-        (a, b) => packageDetails[b] - packageDetails[a],
-      );
-      const packageData = packageLabels.map((label) => packageDetails[label]);
-
-      const topPackageCount = 150;
-      const topPackageLabels = packageLabels.slice(0, topPackageCount);
-      const topPackageData = packageData.slice(0, topPackageCount);
-      const remainingPackageSum = packageData
-        .slice(30)
-        .reduce((a, b) => a + b, 0);
-      allPackageDurationChart.updateOptions({
-        labels: [...topPackageLabels, 'Others'],
-      });
-      allPackageDurationChart.updateSeries([
-        ...topPackageData,
-        remainingPackageSum,
-      ]);
-
-      const buildSelector = document.querySelector('#build-select');
-      buildSelector.value = buildIndex;
-    };
-
-    // Each package duration chart
-    const multiPackageDurationOptions = {
-      series: [],
-      chart: {
-        height: 350,
-        type: 'line',
-        zoom: {
-          enabled: true,
-        },
-        selection: {
-          enabled: true,
-        },
-        events: {
-          click: (_event, _chartContext, config) => {
-            const dataPoint = config.dataPointIndex;
-            if (dataPoint === undefined) {
-              return;
-            }
-
-            showAllPackageDuration(dataPoint);
-          },
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      title: {
-        text: 'Build duration',
-        align: 'left',
-      },
-      grid: {
-        row: {
-          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-          opacity: 0.5,
-        },
-      },
-      xaxis: {
-        type: 'datetime',
-      },
-      yaxis: {
-        labels: {
-          formatter: (val) => val && mmss(val),
-        },
-        title: {
-          text: 'Duration',
-        },
-      },
-      tooltip: {
-        y: {
-          formatter: (val) => val && mmss(val),
-        },
-      },
-    };
-
-    const multiPackageDurationChart = new ApexCharts(
-      document.querySelector('#package-time-chart'),
-      multiPackageDurationOptions,
-    );
-    multiPackageDurationChart.render();
-
-    // Handler
-    const showPackageDuration = (packageName) => {
-      const packageSelector = document.querySelector('#package-select');
-      packageSelector.value = packageName;
-
-      const packageData = validatedWorkflowTime.map((data) => [
-        new Date(data.date),
-        data.details?.[packageName] ?? null,
-      ]);
-
-      multiPackageDurationChart.updateSeries([
-        {
-          name: packageName,
-          data: packageData,
-        },
-      ]);
-    };
-
     // Build duration chart
     const buildDurationOptions = {
       series: [
@@ -188,23 +40,13 @@ fetch('github_action_data.json')
         },
       ],
       chart: {
-        height: 350,
+        height: 500,
         type: 'line',
         zoom: {
           enabled: true,
         },
         selection: {
           enabled: true,
-        },
-        events: {
-          click: (_event, _chartContext, config) => {
-            const dataPoint = config.dataPointIndex;
-            if (dataPoint === undefined) {
-              return;
-            }
-
-            showAllPackageDuration(dataPoint);
-          },
         },
       },
       dataLabels: {
@@ -247,37 +89,6 @@ fetch('github_action_data.json')
     );
     buildDurationChart.render();
 
-    // Build selector
-    const buildSelector = document.querySelector('#build-select');
-    validatedWorkflowTime.forEach((data, index) => {
-      if (data.details !== null) {
-        const option = document.createElement('option');
-        option.value = index;
-        option.text = `${data.date} (${data.duration.toFixed(2)}h)`;
-        buildSelector.appendChild(option);
-      }
-    });
-
-    buildSelector.addEventListener('change', (event) => {
-      showAllPackageDuration(Number(event.target.value));
-    });
-
-    // Package selector
-    const packageSelector = document.querySelector('#package-select');
-
-    Array.from(packageList)
-      .sort()
-      .forEach((key) => {
-        const option = document.createElement('option');
-        option.value = key;
-        option.text = `${key}`;
-        packageSelector.appendChild(option);
-      });
-
-    packageSelector.addEventListener('change', (event) => {
-      showPackageDuration(event.target.value);
-    });
-
     // Docker
     const dockerOptions = {
       series: [
@@ -319,7 +130,7 @@ fetch('github_action_data.json')
         },
       ],
       chart: {
-        height: 350,
+        height: 500,
         type: 'line',
         zoom: {
           enabled: true,
