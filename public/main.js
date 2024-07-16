0cb3676fbf41e6950f9ef959efa373229d1835ce
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-07-16 22:16:57 +0900

    feat: add `docker-build-and-push` chart (#21)
    * update constant
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * fix format
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * rename
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * reorder
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    ---------
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- public/main.js
+++ public/main.js
@@ -1,40 +1,37 @@
 fetch('github_action_data.json')
   .then((res) => res.json())
   .then((json) => {
-    const validatedWorkflowTime = json.workflow_time["health-check"].filter(
+    const healthCheck = json.workflow_time["health-check"].filter(
       (data) => 'no-cuda' in data.jobs && 'cuda' in data.jobs);
-    const validatedWorkflowTimeSelfHosted = json.workflow_time["health-check-self-hosted"].filter(
+    const healthCheckSelfHosted = json.workflow_time["health-check-self-hosted"].filter(
       (data) => 'no-cuda' in data.jobs && 'cuda' in data.jobs);
-    const packageList = new Set(
-      validatedWorkflowTime.flatMap((data) => Object.keys(data.details ?? {})),
-    );
-    const mmss = (seconds) =>
-      `${Math.ceil(seconds / 60)}m${(seconds % 60).toFixed(0)}s`;
+    const dockerBuildAndPush = json.workflow_time["docker-build-and-push"].filter(
+      (data) => 'no-cuda' in data.jobs && 'cuda' in data.jobs);
 
     // Build duration chart
-    const buildDurationOptions = {
+    const healthCheckTimeOptions = {
       series: [
         {
           name: 'health-check (no-cuda)',
-          data: validatedWorkflowTime.map((data) => {
+          data: healthCheck.map((data) => {
             return [new Date(data.date), data.jobs['no-cuda'] / 3600.0];
           }),
         },
         {
           name: 'health-check (cuda)',
-          data: validatedWorkflowTime.map((data) => {
+          data: healthCheck.map((data) => {
             return [new Date(data.date), data.jobs['cuda'] / 3600.0];
           }),
         },
         {
           name: 'health-check-self-hosted (no-cuda)',
-          data: validatedWorkflowTimeSelfHosted.map((data) => {
+          data: healthCheckSelfHosted.map((data) => {
             return [new Date(data.date), data.jobs['no-cuda'] / 3600.0];
           }),
         },
         {
           name: 'health-check-self-hosted (cuda)',
-          data: validatedWorkflowTimeSelfHosted.map((data) => {
+          data: healthCheckSelfHosted.map((data) => {
             return [new Date(data.date), data.jobs['cuda'] / 3600.0];
           }),
         },
@@ -83,11 +80,76 @@ fetch('github_action_data.json')
       },
     };
 
-    const buildDurationChart = new ApexCharts(
-      document.querySelector('#build-time-chart'),
-      buildDurationOptions,
+    const healthCheckTimeChart = new ApexCharts(
+      document.querySelector('#health-check-time-chart'),
+      healthCheckTimeOptions,
     );
-    buildDurationChart.render();
+    healthCheckTimeChart.render();
+
+    const dockerBuildAndPushTimeOptions = {
+      series: [
+        {
+          name: 'docker-build-and-push (no-cuda)',
+          data: dockerBuildAndPush.map((data) => {
+            return [new Date(data.date), data.jobs['no-cuda'] / 3600.0];
+          }),
+        },
+        {
+          name: 'docker-build-and-push (cuda)',
+          data: dockerBuildAndPush.map((data) => {
+            return [new Date(data.date), data.jobs['cuda'] / 3600.0];
+          }),
+        },
+      ],
+      chart: {
+        height: 500,
+        type: 'line',
+        zoom: {
+          enabled: true,
+        },
+        selection: {
+          enabled: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      title: {
+        text: 'Build duration',
+        align: 'left',
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+      yaxis: {
+        min: 0,
+        labels: {
+          formatter: (val) => `${val.toFixed(2)}h`,
+        },
+        title: {
+          text: 'Duration',
+        },
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return `${val.toFixed(2)}h`;
+          },
+        },
+      },
+    };
+
+    const dockerBuildAndPushTimeChart = new ApexCharts(
+      document.querySelector('#docker-build-and-push-time-chart'),
+      dockerBuildAndPushTimeOptions,
+    );
+    dockerBuildAndPushTimeChart.render();
 
     // Docker
     const dockerOptions = {
